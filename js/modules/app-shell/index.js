0861e8314b210e0ccc08cd7f8b29a457bcac2d4f
Eduardo12Pacheco <eduarisrapache@gmail.com>
2026-05-19 21:53:21 -0500

    fix: bust newspaper app shell entry cache

--- js/modules/app-shell/index.js
+++ js/modules/app-shell/index.js
@@ -1,1 +1,1 @@
-export { bootApp, bootCompatibilityShell, __testHooks } from './runtime.js?v=20260519-project-card-polish';
+export { bootApp, bootCompatibilityShell, __testHooks } from './runtime.js?v=20260520-newspaper-effect';
